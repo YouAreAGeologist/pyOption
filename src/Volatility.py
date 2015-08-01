39df460f8cd7a60f18e8bd77eedb8c1298fd3ace
George Marklow <george.marklow@hotmail.co.uk>
2015-08-01 15:39:46 +0100

    Get high low close volatility

--- src/Volatility.py
+++ src/Volatility.py
@@ -15,6 +15,7 @@ class Volatility:
         
         return math.sqrt((sum1/(n-1)) - ((math.pow(close_prices[i]/close_prices[i-1],2)/(n * (n-1)))))
         
+    # Parkinson (1980)
     @staticmethod
     def get_high_low_volatility(low_prices,high_prices):
         
@@ -24,3 +25,19 @@ class Volatility:
             sum1 += high_prices(i)/low_prices(i)
         
         return sum1/(2 * n * math.sqrt(math.log(2)))
+        
+    # Garman and Klass (1980)
+    def get_high_low_close_volatility(close_prices,low_prices,high_prices):
+        
+        n = len(close_prices)
+        sum1 = 0
+        sum2 = 0
+        
+        for i in (1,n):
+            sum1 += 0.5 * math.pow(math.log(high_prices[i]/low_prices[i]) ,2)
+            sum2 += ((2 * math.log(2) - 1) * math.pow(math.log(close_prices[i]/close_prices[i-1]),2))
+            
+        return math.sqrt((sum1 - sum2)/n)
+        
+    
+        
