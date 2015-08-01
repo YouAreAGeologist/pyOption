daa5d04dd685831e98c019c1ee78f7417686c58e
George Marklow <george.marklow@hotmail.co.uk>
2015-08-01 14:38:41 +0100

    Get historical volatility from close prices

--- src/Volatility.py
+++ src/Volatility.py
@@ -1,2 +1,18 @@
 # Volatility functions library
 
+class Volatility:
+    
+    @staticmethod
+    def get_historical_volatility_from_close_prices(close_prices):
+        result = None
+        n = len(close_prices)
+        sum1 = 0
+        sum2 = 0
+        
+        for i in (1,n):
+            sum1 += math.pow(math.log(close_prices[i]/close_prices[i-1]),2)
+            sum2 += math.log(close_prices[i]/close_prices[i-1])
+        
+        return math.sqrt((sum1/(n-1)) - ((math.pow(close_prices[i]/close_prices[i-1],2)/(n * (n-1)))))
+        
+    
