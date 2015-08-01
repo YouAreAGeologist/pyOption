c8bba3a8de7d3819c7b4479c1616282b2ccc4e27
George Marklow <george.marklow@hotmail.co.uk>
2015-08-01 16:40:31 +0100

    Box Muller method

--- src/NumericalGreeks.py
+++ src/NumericalGreeks.py
@@ -1,2 +1,7 @@
 # Greeks by finite difference approximations
 
+class NumericalGreeks:
+    
+    @staticmethod
+    def get_delta(price,price_lower,price_upper,ds):
+        return (price_upper - price_lower)/(2 * ds)
--- src/Random.py
+++ src/Random.py
@@ -1,11 +1,24 @@
 # Ramdom number generation static functions
 
+import random
+
 class Random:
 
-    # Ideally return an array of random numbers as will be
-    # used in Monte-Carlo simulation
-    def get_box_muller_random_number():
+    def get_gaussian_box_muller_values(n):
         
-        # return array for input collection size
-        return 0
+        x = 0
+        y = 0
+        euclid_sq = 0
+        values = []
+
+        for i in n:
+            while True:
+                x = 2 * random.random()
+                y = 2 * random.random()
+                euclid_sq = math.pow(x,2) + math.pow(y,2)
+                if euclid_sq < 1:
+                    values.append(euclid_sq)
+                    break
+                
+        return values
         
