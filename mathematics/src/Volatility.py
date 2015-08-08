f1a2de22dbfdf1618cf943e63e340550dc2be4c7
George Marklow <george.marklow@hotmail.co.uk>
2015-08-08 13:38:56 +0100

    Update Volatility.py

--- mathematics/src/Volatility.py
+++ mathematics/src/Volatility.py
@@ -12,7 +12,6 @@ def get_historical_volatility_from_close_prices(close_prices):
     return math.sqrt((sum1/(n-1)) - ((math.pow(close_prices[i]/close_prices[i-1],2)/(n * (n-1)))))
 
 
-# Parkinson (1980)
 def get_high_low_volatility(low_prices,high_prices):
 
     n = len(low_prices)
@@ -20,18 +19,3 @@ def get_high_low_volatility(low_prices,high_prices):
     for i in (1,n):
         sum1 += high_prices(i)/low_prices(i)
     return sum1/(2 * n * math.sqrt(math.log(2)))
-
-
-# Garman and Klass (1980)
-def get_high_low_close_volatility(close_prices,low_prices,high_prices):
-
-    n = len(close_prices)
-    sum1, sum2 = 0
-    for i in (1,n):
-        sum1 += 0.5 * math.pow(math.log(high_prices[i]/low_prices[i]) ,2)
-        sum2 += ((2 * math.log(2) - 1) * math.pow(math.log(close_prices[i]/close_prices[i-1]),2))
-    return math.sqrt((sum1 - sum2)/n)
-
-
-
-
