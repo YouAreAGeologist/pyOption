a3e682fd5931c1ebf84b7ca45b62f6080a48b680
George Marklow <george.marklow@hotmail.co.uk>
2015-08-11 21:48:33 +0100

    Update distribution.py

--- mathematics/test/distribution.py
+++ mathematics/test/distribution.py
@@ -1,1 +1,16 @@
+import unittest
+import math
+from mathematics.src.distribution import *
+
+class Distribution(unittest.TestCase):
+
+    def test_ndf(self):
+        self.assertEqual(round(ndf(0.2387), 4), 0.3877)
+        self.assertEqual(round(ndf(1.3446), 4), 0.1616)
+        self.assertEqual(round(ndf(0.0837), 4), 0.3975)
+
+    def test_cnd(self):
+        self.assertEqual(round(cnd(0), 4), 0.5000)
+        self.assertEqual(round(cnd(-1.0), 4), 0.1587)
+        self.assertEqual(round(cnd(1.0), 4), 0.8413)
 
