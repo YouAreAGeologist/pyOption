9dabf2f51b0253672972739569eb3ff70d2d9225
gmarklow87 <george.marklow"hotmail.co.uk>
2015-08-13 22:37:29 +0100

    GM: Tidy up

--- core/src/standard_barrier_option.py
+++ core/src/standard_barrier_option.py
@@ -1,7 +1,7 @@
 # Standard Barrier Option pricing
 
 class StanadardBarrierOption:
-    
+
     def __init__(self,flag,barrierType,s,x,k,r,b,t,sigma):
         self.flag = flag
         self.barrierType = barrierType
@@ -21,14 +21,14 @@ class StanadardBarrierOption:
         self.z = (math.log(h/s)/(sigma * math.sqrt(t))) + (lmd * sigma * math.sqrt(t))
         self.eta = None
         self.phi = None
-    
+
     # Standard barrier option price
     def get_price():
         result = None
-        
+
         # document all barrier types up and in call etc
         #
-        
+
         if barrierType == "in":
             if flag == 'c':
                 if s > h:
@@ -50,7 +50,7 @@ class StanadardBarrierOption:
                     phi = 1
                     eta = -1
                     if x > h:
-                        result = __b() - __c() + __d() + __e() 
+                        result = __b() - __c() + __d() + __e()
                     elif x < h:
                         result = __a() + __e()
                 elif s < h:
@@ -92,23 +92,22 @@ class StanadardBarrierOption:
                     elif x < h:
                         result = __a() - __c() + __f()
         return result
-        
+
     def __a():
         return (phi * s * math.exp((b-r)*t) * cnd(phi * x1)) - (phi * x * math.exp(-r*t) * cnd((phi * x1) - (phi * sigma * math.sqrt(t))))
-        
+
     def __b():
         return (phi * s * math.exp((b-r)*t) * cnd(phi * x2)) - (phi * x * math.exp(-r*t) * cnd((phi * x2) - (phi * sigma * math.sqrt(t))))
-        
+
     def __c():
         return (phi * s * math.exp((b-r)*t) * math.pow(h/s,2*(mu + 1)) * cnd(eta * y1)) - (phi * x * math.exp(-r * t) * math.pow(h/s,2*mu) * cnd((eta * y1) - (eta * sigma * math.sqrt(t)))
-        
+
     def __d():
         return (phi * s * math.exp((b-r)*t) * math.pow(h/s,2*(mu + 1)) * cnd(eta * y1)) - (phi * x * math.exp(-r * t) * math.pow(h/s,2*mu) * cnd((eta * y1) - (eta * sigma * math.sqrt(t)))
-        
+
     def __e():
-        return (k * math.exp(-r*t)) * (cnd((eta * x2) - (eta * sigma * math.sqrt(t))) - (math.pow(h/s,2 * mu) * (cnd((eta * y2) - (eta * sigma * math.sqrt(t)))))) 
-        
+        return (k * math.exp(-r*t)) * (cnd((eta * x2) - (eta * sigma * math.sqrt(t))) - (math.pow(h/s,2 * mu) * (cnd((eta * y2) - (eta * sigma * math.sqrt(t))))))
+
     def __f():
         return k * ((math.pow(h/s, mu+lmd) * cnd(eta * z)) + (math.pow(h/s,mu-lmd) * cnd((eta * z) - (2 * eta * lmd * sigma * math.sqrt(t)))))
-    
-        
+
