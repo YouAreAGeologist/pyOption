bd33c8096303947659f395b2ebd64e51260a573f
gmarklow87 <george.marklow"hotmail.co.uk>
2015-08-16 22:18:15 +0100

    GM: asset-or-nothing option inc unit test

---
+++ core/src/asset_or_nothing_option.py
@@ -0,0 +1,25 @@
+import math
+from mathematics.src.distribution import cnd
+
+
+class AssetOrNothingOption:
+
+
+    def __init__(self, flag, s, x, r, b, t, sigma):
+        self.__flag = flag
+        self.__s = s
+        self.__x = x
+        self.__r = r
+        self.__b = b
+        self.__t = t
+        self.__sigma = sigma
+        self.__d = (math.log(s/x) + (b + math.pow(sigma, 2)/2) * t) / (sigma * math.sqrt(t))
+
+
+    def get_price(self):
+        result=None
+        if self.__flag == 'c':
+            result = self.__s * math.exp((self.__b - self.__r) * self.__t) * cnd(self.__d)
+        elif self.__flag == 'p':
+            result = self.__s * math.exp((self.__b - self.__r) * self.__t) * cnd(-self.__d)
+        return result
