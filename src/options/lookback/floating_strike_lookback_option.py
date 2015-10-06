43108719296f20b2c60e66eacaf389f8e0e458f5
User <george.marklow@hotmail.co.uk>
2015-10-06 21:50:11 +0100

    GM

--- src/options/lookback/floating_strike_lookback_option.py
+++ src/options/lookback/floating_strike_lookback_option.py
@@ -1,42 +1,42 @@
 import math
-import logging
 from src.options.option_base import OptionBase
 from src.mathematics.distributions.cumulative_normal_distribution import N
-
-logger = logging.getLogger(config.settings['logger_name'])
 
 
 class FlostingStrikeLookbackOption(OptionBase):
     def __init__(self, flag, s, s_min, s_max, x, r, b, t, sigma):
-        super(FlostingStrikeLookbackOption, self).__init__(flag)
-        logger.info('FloatingStrikeLookbackOption.ctor')
+        super(FlostingStrikeLookbackOption, self).__init__()
+        self.flag = flag
+        self.s = s
+        self.s_min = s_min
+        self.s_max = s_max
+        self.x = x
+        self.r = r
+        self.b = b
         self.t = t
+        self.sigma = sigma
 
     def get_value(self):
-        logger.info('FloatingStrikeLookbackOption.get_value')
         result = None
-        try:
-            if self.flag == 'call':
-                a1 = (math.log(self.s / self.s_min) + (self.b + 0.5 * math.pow(self.sigma, 2)) * self.t) / (
-                    self.sigma * math.sqrt(self.t))
-                a2 = a1 - self.sigma * math.sqrt(self.t)
-                result = self.s * math.exp((self.b - self.r) * self.t) * N(a1) - (
-                    self.s_min * math.exp(-self.r * self.t) * N(a2)) + (
-                             (self.s * math.exp(-self.r * self.t) * 0.5 * math.pow(self.sigma, 2) / self.b) * (
-                                 math.pow(self.s / self.s_min, -2 * self.b / math.pow(self.sigma, 2)) * N(
-                                     -a1 + (2 * self.b * math.sqrt(self.t) / self.sigma) - (
-                                         math.exp(self.b * self.t) * N(-a1)))))
-            elif self.flag == 'put':
-                b1 = (math.log(self.s / self.s_min) + (self.b + 0.5 * math.pow(self.sigma, 2)) * self.t) / (
-                    self.sigma * math.sqrt(self.t))
-                b2 = b1 - self.sigma * math.sqrt(self.t)
-                result = self.s_max * math.exp(-self.r * self.t) * N(-b2) - (
-                    self.s * math.exp((self.b - self.r) * self.t) * N(-b1)) + (
-                             self.s * math.exp(-self.r * self.t) * math.pow(self.sigma, 2) / (2 * self.b)) + (
-                             (self.s * math.exp(-self.r * self.t) * math.pow(self.sigma, 2) / (2 * self.b)) * (-(
-                                 math.pow(self.s / self.s_max, -2 * self.b / math.pow(self.sigma, 2)) * N(
-                                     b1 - 2 * self.b * math.sqrt(self.t) / self.sigma)) + (math.exp(
-                                 self.b * self.t) * N(b1))))
-        except Exception, e:
-            logger.error(e)
+        if self.flag == 'call':
+            a1 = (math.log(self.s / self.s_min) + (self.b + 0.5 * math.pow(self.sigma, 2)) * self.t) / (
+                self.sigma * math.sqrt(self.t))
+            a2 = a1 - self.sigma * math.sqrt(self.t)
+            result = self.s * math.exp((self.b - self.r) * self.t) * N(a1) - (
+                self.s_min * math.exp(-self.r * self.t) * N(a2)) + (
+                         (self.s * math.exp(-self.r * self.t) * 0.5 * math.pow(self.sigma, 2) / self.b) * (
+                             math.pow(self.s / self.s_min, -2 * self.b / math.pow(self.sigma, 2)) * N(
+                                 -a1 + (2 * self.b * math.sqrt(self.t) / self.sigma) - (
+                                     math.exp(self.b * self.t) * N(-a1)))))
+        elif self.flag == 'put':
+            b1 = (math.log(self.s / self.s_min) + (self.b + 0.5 * math.pow(self.sigma, 2)) * self.t) / (
+                self.sigma * math.sqrt(self.t))
+            b2 = b1 - self.sigma * math.sqrt(self.t)
+            result = self.s_max * math.exp(-self.r * self.t) * N(-b2) - (
+                self.s * math.exp((self.b - self.r) * self.t) * N(-b1)) + (
+                         self.s * math.exp(-self.r * self.t) * math.pow(self.sigma, 2) / (2 * self.b)) + (
+                         (self.s * math.exp(-self.r * self.t) * math.pow(self.sigma, 2) / (2 * self.b)) * (-(
+                             math.pow(self.s / self.s_max, -2 * self.b / math.pow(self.sigma, 2)) * N(
+                                 b1 - 2 * self.b * math.sqrt(self.t) / self.sigma)) + (math.exp(
+                             self.b * self.t) * N(b1))))
         return result
--- src/options/option_base.py
+++ src/options/option_base.py
@@ -2,20 +2,12 @@ from abc import ABCMeta, abstractmethod
 import src.config as config
 import logging
 
-logger = logging.getLogger(config.settings['logger_name'])
-hdlr = logging.FileHandler(config.settings['logger_path'])
-formatter = logging.Formatter(config.settings['logger_format'])
-hdlr.setFormatter(formatter)
-logger.addHandler(hdlr)
-logger.setLevel(logging.WARNING)
-
 class OptionBase(object):
     __metaclass__ = ABCMeta
 
-    def __init__(self, flag):
-        self.logger.info('OptionBase.__init__')
-        self.flag = flag
+    def __init__(self):
+        pass
 
     @abstractmethod
     def get_value(self):
-        self.logger.info('OptionBase.get_value')
+        pass
