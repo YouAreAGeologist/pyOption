d7a8727d0d454aa9c0015f356861f160a5097b87
User <george.marklow@hotmail.co.uk>
2015-10-07 21:42:43 +0100

    gm

--- test/test.py
+++
@@ -1,9 +0,0 @@
-from src.options.european.vanilla_european_option import VanillaEuropeanOption
-from src.greeks.calculators.numerical_greeks_calculator import NumericalGreeksCalculator
-
-
-option = VanillaEuropeanOption('call', 100.0, 95.0, 0.1, 0.05, 0.5, 0.2)
-print(option.get_value())
-
-greeks = NumericalGreeksCalculator(option)
-greeks.get_values(('delta', 'gamma'))
