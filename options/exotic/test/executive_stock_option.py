25d235064a41a446ed886a9f0045bae4a4f5c685
User <george.marklow@hotmail.co.uk>
2015-10-18 11:21:56 +0100

    gm

--- options/exotic/test/executive_stock_option.py
+++ options/exotic/test/executive_stock_option.py
@@ -1,1 +1,17 @@
-__author__ = 'User'
+import unittest
+from options.exotic.src.executive_stock_option import ExecutiveStockOption
+
+
+class ExecutiveStockOptionTest(unittest.TestCase):
+    def setUp(self):
+        self.call_option = ExecutiveStockOption('put', 60.0, 64.0, 0.07, 0.04, 2.0, 0.15, 0.38)
+
+    def test_call_option(self):
+        self.assertEqual(round(self.call_option.get_value(), 4), 9.1244)
+
+    def test_put_option(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
--- options/exotic/test/mirror_option.py
+++ options/exotic/test/mirror_option.py
@@ -1,1 +1,26 @@
-__author__ = 'User'
+import unittest
+from options.exotic.src.mirror_option import MirrorOption
+
+
+class FadeInOptionTest(unittest.TestCase):
+    def setUp(self):
+        self.s = 100.0
+        self.t = 0.5
+        self.r = 0.1
+        self.b = 0.0
+
+    def test_long_call_option(self):
+        self.assertEqual(round(MirrorOption('call', 'long', self.s, 95.0, self.r, self.b, self.t, 2.0).get_value(), 4), 15.1251)
+
+    def test_long_put_option(self):
+        self.assertEqual(round(MirrorOption('put', 'long', self.s, 95.0, self.r, self.b, self.t, 0.2).get_value(), 4), 3.2476)
+
+    def test_short_call_option(self):
+        self.assertEqual(round(MirrorOption('call', 'short', self.s, 95.0, self.r, self.b, self.t, 2.0).get_value(), 4), -12.2959)
+
+    def test_short_put_option(self):
+        self.assertEqual(round(MirrorOption('put', 'short', self.s, 95.0, self.r, self.b, self.t, 0.2).get_value(), 4), -2.3841)
+
+
+if __name__ == '__main__':
+    unittest.main()
